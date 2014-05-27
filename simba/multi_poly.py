3476ce6f608826db979105c59e484e32d2c12027
vivitas <fvivitas@gmail.com>
2014-05-27 21:09:31 +0200

    Added comments for most functions

--- simba/multi_poly.py
+++ simba/multi_poly.py
@@ -30,6 +30,9 @@ def constant(monom):
 		return monom[K_NAME_FOR_CONSTANT]
 	return 1
 
+'''
+
+'''
 def multiorder(monom, all_degrees=False):
 	result = []
 	if all_degrees:
@@ -46,9 +49,26 @@ def multiorder(monom, all_degrees=False):
 		result.append(order)
 	return result
 
+'''
+Checks if two monoms are same.
+
+[in]		first_monom
+	monom
+
+[in]		second_monom
+	monom
+
+[in]		constant_matter
+	if true than monoms that differ only by multiplicative constant are not considered same
+
+[returns]	True if monoms are same and False otherwise
+'''
 def same(first_monom, second_monom, constant_matter=True):
 	return multiorder(first_monom, True) == multiorder(second_monom, True) and (not constant_matter or (constant(first_monom) == constant(second_monom)))
 
+'''
+Comparator function that is used to sort monomials in graded lex order
+'''
 def grad_compare(first_monom, second_monom):
 	first_order = sum(multiorder(first_monom))
 	second_order = sum(multiorder(second_monom))
@@ -56,6 +76,18 @@ def grad_compare(first_monom, second_monom):
 		return lex_compare(first_monom, second_monom)
 	return second_order - first_order
 
+'''
+Calculates sum of two polynomials
+
+[in]		first_poly
+	polynomial
+
+[in]		second_poly
+	polynomial
+
+[returns]	result
+	first_poly + second_poly
+'''
 def add(first_poly, second_poly):
 	result = copy.deepcopy(first_poly)
 	for monom_second in second_poly:
@@ -73,15 +105,48 @@ def add(first_poly, second_poly):
 			final_result.append(monom)
 	return final_result
 
+'''
+Calculates polynomial opposite (addition) to given polynomial
+
+[in]		poly
+	polynomial
+
+[returns]	result
+	-poly
+'''
 def unary_minus(poly):
 	result = copy.deepcopy(poly)
 	for monom in result:
 		monom[K_NAME_FOR_CONSTANT] = -constant(monom)
 	return result
 
+'''
+Calculates difference between two polynomials
+
+[in]		poly_first
+	polynomial
+
+[in]		poly_second
+	polynomial
+
+[returns]	result
+	poly_first - poly_second
+'''
 def minus(poly_first, poly_second):
 	return add(poly_first, unary_minus(poly_second))
 
+'''
+Prints monome
+
+[in]		monom
+	monom to be printed
+
+[in]		new_line
+	whether new line should be printed after monom, default false
+
+[in]		print_sorted
+	whether variables shold be sorted inside monomial
+'''
 def print_monome(monom, new_line=False, print_sorted=True):
 	if K_NAME_FOR_CONSTANT in monom:
 		if monom[K_NAME_FOR_CONSTANT] != 0:
@@ -101,6 +166,14 @@ def print_monome(monom, new_line=False, print_sorted=True):
 	if new_line:
 		print
 
+'''
+Tries to remove unnecessary monoms from polynomial.
+
+[in]		poly
+	polynomial
+[returns]	poly
+	resulting polynomial, possibly without some unnecessary monoms
+'''
 def poly_update(poly):
 	updated = []
 	for monom in poly:
@@ -111,12 +184,24 @@ def poly_update(poly):
 				updated.append(copy.deepcopy(monom))
 	return updated
 
+'''
+Prints polynomial
+
+[in]		poly
+	polynomial to be printed
+
+[in]		new_line
+	whether new line should be printed after polynomial
+
+[in]		print_sorted
+	whether variables should be sorted in any way for printing purposes
+'''
 def print_poly(poly, new_line=False, print_sorted=True):
 	poly = poly_update(poly)
 	first = True
 	for monom in poly:
 		if (first):
-			print_monome(monom, print_sorted = print_sorted),
+			print_monome(monom, False, print_sorted)
 		else:
 			print("+"),
 			print_monome(monom, False, print_sorted)
@@ -124,11 +209,34 @@ def print_poly(poly, new_line=False, print_sorted=True):
 	if new_line:
 		print
 
+'''
+Returns degree of variable in monom
+
+[in]		monom
+	monom
+
+[in]		variable
+	variable
+
+[returns]	degree
+	degree of variable in monom; if variable does not appear in monom - 0
+'''
 def order_of(monom, variable):
 	if variable not in monom:
 		return 0
 	return monom[variable]
 
+'''
+Get's leading term of polynomial in given monomial order
+
+[in]		poly
+	polynomial
+[in]		order
+	monomial order
+
+[returns]	result
+	monom - copy of leading term of poly in order order
+'''
 def leading_term(poly, order):
 	if len(poly) == 0:
 		return copy.deepcopy(K_ZERO_MONOM)
@@ -136,6 +244,18 @@ def leading_term(poly, order):
 	tmp.sort(cmp=order)
 	return tmp[0]
 
+'''
+Multiplies two monoms
+
+[in]		monom_a
+	monom to be multiplied
+
+[in]		monom_b
+	monom to be multiplied
+
+[returns]	result
+	monom, result of multiplication
+'''
 def monom_multiply(monom_a, monom_b):
 	result = copy.deepcopy(K_ZERO_MONOM)
 	for variable in string.ascii_letters:
@@ -146,18 +266,53 @@ def monom_multiply(monom_a, monom_b):
 	result[K_NAME_FOR_CONSTANT] = constant(monom_a) * constant(monom_b)
 	return result
 
+'''
+Multiplies monom by polynomial
+
+[in]		monom
+	monom to be multiplied
+
+[in]		poly
+	polynomial to be multiplied
+
+[returns]	result
+	polynomial, result of multiplication
+'''
 def poly_monom_multiply(monom, poly):
 	result = copy.deepcopy(K_ZERO_POLY)
 	for monom_in_poly in poly:
 		result = add([monom_multiply(monom, monom_in_poly)], result)
 	return result
 
+'''
+Multiplies two polynomials
+
+[in]		poly_a
+	first polynomial
+
+[in]		poly_b
+	second polynomial
+
+[returns]	result
+	result of division
+'''
 def poly_multiply(poly_a, poly_b):
 	result = copy.deepcopy(K_ZERO_POLY)
 	for monom_in_a in poly_a:
 		result = add(result, poly_monom_multiply(monom_in_a, poly_b))
 	return result
 
+'''
+Checks if monom is divisible with another monom
+
+[in]		monom_a
+	monom to be divided
+
+[in]		monom_b
+	monom to be divided with
+
+[returns]	True if divisible; False otherwise
+'''
 def divides(monom_a, monom_b):
 	for variable in monom_b:
 		if variable == K_NAME_FOR_CONSTANT:
@@ -168,6 +323,18 @@ def divides(monom_a, monom_b):
 			return False
 	return True
 
+'''
+Divides two monoms if they are divisible. If not behaviour is undefined.
+
+[in]		monom_a
+	monom that is to be divided
+
+[in]		monom_b
+	monom which is divisor
+
+[returns]	result
+	result of division
+'''
 def monom_divide(monom_a, monom_b):
 	result = copy.deepcopy(K_ZERO_MONOM)
 	for variable in monom_a:
@@ -177,6 +344,22 @@ def monom_divide(monom_a, monom_b):
 			result[variable] = monom_a[variable] - order_of(monom_b, variable)
 	return result
 
+'''
+Divides two polynomials in given order. If no order is given lex_compare is assumed.
+
+[in]		poly
+	polynomial to be divided
+
+[in]		poly_list
+	list of polynomials that divide polynomial
+
+[in]		order
+	monomial order in which division will be done; if none given lex_compare is assumed
+
+[returns]	(quotient, remainder):
+	quotient is list of quotients that are calculated such that quotiend[i] coresponds to poly_list[i]
+	remainder is single poly that is remainder	
+'''
 def poly_divide(poly, poly_list, order=lex_compare):
 	quotient = [copy.deepcopy(K_ZERO_POLY)] * len(poly_list)
 	remainder = copy.deepcopy(K_ZERO_POLY)
